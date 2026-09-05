1b4f0f4c7537532b3b034db05d81651a95adbb4f
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-09-06 00:17:20 +0530

    test: update widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mine/app/app.dart';
 import 'package:mine/core/crypto/crypto_service.dart';
@@ -5,12 +6,14 @@ import 'package:mine/core/storage/app_database.dart';
 import 'package:mine/core/storage/secure_key_store.dart';
 import 'package:mine/data/repositories/chat_repository.dart';
 import 'package:mine/data/repositories/contact_repository.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
   setUpAll(() {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
+    FlutterSecureStorage.setMockInitialValues({});
   });
 
   testWidgets('Mine App boots up and displays identity initialization', (WidgetTester tester) async {
@@ -31,8 +34,11 @@ void main() {
       ),
     );
 
-    // Initial check
+    await tester.pump(const Duration(milliseconds: 100));
     expect(find.byType(MineApp), findsOneWidget);
+
+    // Unmount widget to clean up timers
+    await tester.pumpWidget(const SizedBox());
     await rawDb.close();
   });
 }

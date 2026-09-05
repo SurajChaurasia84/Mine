e1cc9b5b1a9d14081fcf96abfbfbbb65f9dccd50
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-09-06 00:17:11 +0530

    feat: update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'dart:io';
+import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'app/app.dart';
 import 'core/crypto/crypto_service.dart';
 import 'core/storage/app_database.dart';
@@ -13,7 +14,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Desktop or test runner SQLite fallback
-  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
   }

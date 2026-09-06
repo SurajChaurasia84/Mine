95cf71847767b0283ed26c52a0a8163cbf8d12e2
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-09-06 13:16:54 +0530

    Update app theme

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MineTheme {
   // WhatsApp-inspired privacy palette
   static const Color primaryTeal = Color(0xFF00A884);
   static const Color primaryDark = Color(0xFF128C7E);
-  static const Color backgroundDark = Color(0xFF121B22);
+  static const Color backgroundDark = Color(0xFF0B1014);
   static const Color surfaceDark = Color(0xFF1F2C34);
   static const Color outgoingBubble = Color(0xFF005C4B);
   static const Color incomingBubble = Color(0xFF202C33);
@@ -20,12 +21,12 @@ class MineTheme {
       scaffoldBackgroundColor: backgroundDark,
       pageTransitionsTheme: const PageTransitionsTheme(
         builders: {
-          TargetPlatform.android: WhatsAppFadePageTransitionsBuilder(),
-          TargetPlatform.iOS: WhatsAppFadePageTransitionsBuilder(),
-          TargetPlatform.windows: WhatsAppFadePageTransitionsBuilder(),
-          TargetPlatform.macOS: WhatsAppFadePageTransitionsBuilder(),
-          TargetPlatform.linux: WhatsAppFadePageTransitionsBuilder(),
-          TargetPlatform.fuchsia: WhatsAppFadePageTransitionsBuilder(),
+          TargetPlatform.android: ZoomPageTransitionsBuilder(),
+          TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+          TargetPlatform.windows: ZoomPageTransitionsBuilder(),
+          TargetPlatform.macOS: CupertinoPageTransitionsBuilder(),
+          TargetPlatform.linux: ZoomPageTransitionsBuilder(),
+          TargetPlatform.fuchsia: ZoomPageTransitionsBuilder(),
         },
       ),
       colorScheme: const ColorScheme.dark(
@@ -37,7 +38,7 @@ class MineTheme {
         surfaceTint: Colors.transparent,
       ),
       appBarTheme: const AppBarTheme(
-        backgroundColor: surfaceDark,
+        backgroundColor: backgroundDark,
         foregroundColor: textLight,
         surfaceTintColor: Colors.transparent,
         scrolledUnderElevation: 0,
@@ -50,10 +51,13 @@ class MineTheme {
           letterSpacing: 0.2,
         ),
       ),
-      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
         backgroundColor: primaryTeal,
-        foregroundColor: Colors.white,
-        elevation: 4,
+        foregroundColor: const Color(0xFF00382B),
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
       ),
       cardTheme: CardThemeData(
         color: surfaceDark,
@@ -67,28 +71,6 @@ class MineTheme {
         color: Color(0xFF222D34),
         thickness: 0.6,
       ),
-    );
-  }
-}
-
-class WhatsAppFadePageTransitionsBuilder extends PageTransitionsBuilder {
-  const WhatsAppFadePageTransitionsBuilder();
-
-  @override
-  Widget buildTransitions<T>(
-    PageRoute<T> route,
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child,
-  ) {
-    return FadeTransition(
-      opacity: CurvedAnimation(
-        parent: animation,
-        curve: Curves.easeInOutCubic,
-        reverseCurve: Curves.easeInOutCubic,
-      ),
-      child: child,
     );
   }
 }

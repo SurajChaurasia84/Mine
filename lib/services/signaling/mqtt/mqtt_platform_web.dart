9df6e7b30976713532fbbf5fd2802737421c66d5
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-09-07 17:27:12 +0530

    Update MQTT web signaling

--- lib/services/signaling/mqtt/mqtt_platform_web.dart
+++ lib/services/signaling/mqtt/mqtt_platform_web.dart
@@ -2,10 +2,28 @@ import 'package:mqtt_client/mqtt_browser_client.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 
 MqttClient createPlatformClient(String broker, String clientId) {
-  final cleanBroker = broker.replaceAll('ws://', '').replaceAll('wss://', '').split('/').first;
-  final wsUrl = 'ws://$cleanBroker:8000/mqtt';
-  final client = MqttBrowserClient(wsUrl, clientId);
-  client.port = 8000;
-  client.websocketProtocols = MqttClientConstants.protocolsSingleDefault;
+  final cleanBroker = broker
+      .replaceAll('ws://', '')
+      .replaceAll('wss://', '')
+      .split('/')
+      .first
+      .split(':')
+      .first;
+
+  String wsUrl;
+  int port;
+  if (cleanBroker.contains('emqx.io')) {
+    wsUrl = 'wss://$cleanBroker:8084/mqtt';
+    port = 8084;
+  } else if (cleanBroker.contains('hivemq.com')) {
+    wsUrl = 'ws://$cleanBroker:8000/mqtt';
+    port = 8000;
+  } else {
+    wsUrl = 'wss://$cleanBroker/mqtt';
+    port = 8084;
+  }
+
+  final client = MqttBrowserClient.withPort(wsUrl, clientId, port);
+  client.websocketProtocols = ['mqtt', 'mqttv3.1', 'mqttv3.1.1'];
   return client;
 }
